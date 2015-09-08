c391dccbd2af534d8490ceb02a02f986a9a98591
David Eads <davideads@gmail.com>
2015-09-08 17:54:08 -0400

    all regions

--- process.py
+++ process.py
@@ -29,7 +29,8 @@ COLUMNS = (
 
 NORTHEAST_STATES = ('CT', 'MA', 'ME', 'NH', 'NJ', 'NY', 'PA', 'RI', 'VT',)
 MIDWEST_STATES = ('IN', 'IL', 'MI', 'OH', 'WI', 'IA', 'KS', 'MN', 'MO', 'NE', 'ND', 'SD',)
-REGIONS = (('northeast', NORTHEAST_STATES), ('midwest', MIDWEST_STATES))
+SOUTH_STATES = ('AR', 'AL', 'DC', 'DE', 'FL', 'GA', 'KY', 'LA', 'MD', 'MS', 'NC', 'OK', 'SC', 'TN', 'TX', 'VA', 'WV',)
+WEST_STATES = ('AK', 'AZ', 'CA', 'CO', 'HI', 'ID', 'MT', 'NM', 'NV', 'OR', 'UT', 'WA', 'WY',)
 
 # 1965 pop: https://www.census.gov/prod/1/pop/p25-420.pdf
 # 2014 pop: http://www.census.gov/popclock/
@@ -41,17 +42,28 @@ MIDWEST_POP = {
     '1965': Decimal(54185000),
     '2014': Decimal(67745108),
 }
+SOUTH_POP = {
+    '1965': Decimal(60166000),
+    '2014': Decimal(119771934),
+}
+WEST_POP = {
+    '1965': Decimal(32036000),
+    '2014': Decimal(75187681),
+}
+
+REGIONS = (
+    ('northeast', NORTHEAST_STATES, NORTHEAST_POP),
+    ('midwest', MIDWEST_STATES, MIDWEST_POP),
+    ('south', SOUTH_STATES, SOUTH_POP),
+    ('west', WEST_STATES, WEST_POP),
+)
+
 
 def summarize():
     table_1965_raw = agate.Table.from_csv('processed-data/1965.csv', COLUMNS)
     table_2015_raw = agate.Table.from_csv('processed-data/2015.csv', COLUMNS)
 
-    for region, states in REGIONS:
-        if region == 'midwest':
-            pop = MIDWEST_POP
-        else:
-            pop = NORTHEAST_POP
-
+    for region, states, population in REGIONS:
         table_1965 = table_1965_raw.where(lambda row: row['state'] in states)
         table_2015 = table_2015_raw.where(lambda row: row['state'] in states)
 
@@ -61,9 +73,9 @@ def summarize():
             row = OrderedDict()
             row['var'] = col_name
             row['1965'] = table_1965.columns[col_name].aggregate(agate.Sum())
-            row['1965_per_capita'] = row['1965'] / pop['1965']
+            row['1965_per_capita'] = row['1965'] / population['1965']
             row['2015'] = table_2015.columns[col_name].aggregate(agate.Sum())
-            row['2015_per_capita'] = row['2015'] / pop['2014']
+            row['2015_per_capita'] = row['2015'] / population['2014']
             row['absolute_percent_change'] = (row['2015'] - row['1965']) / row['1965']
             row['per_capita_percent_change'] = (row['2015_per_capita'] - row['1965_per_capita']) / row['1965_per_capita']
 
